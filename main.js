6d35945bc7ff121649cca32a6241da218b987b76
matas3 <matuska310@gmail.com>
2026-04-23 22:05:08 +0300

    Header Responsiveness

---
+++ main.js
@@ -0,0 +1,8 @@
+function openHamburgerMenu() {
+	document
+		.querySelector(".header-navigation-mobile")
+		.classList.remove("hidden");
+}
+function closeHamburgerMenu() {
+	document.querySelector(".header-navigation-mobile").classList.add("hidden");
+}
